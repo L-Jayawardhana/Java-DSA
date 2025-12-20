5cecb4215269ae42c1dfcc14e4288130fa554ac7
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-12-21 01:58:14 +0530

    add a description to main class about the programme

--- Arrays/src/TwoPointers/Main.java
+++ Arrays/src/TwoPointers/Main.java
@@ -2,6 +2,23 @@ package TwoPointers;
 
 import java.util.Scanner;
 
+/**
+ * Two Sum Problem (Sorted Array) - Two Pointer Technique
+ *
+ * Problem: Given a sorted array and a target sum, find if there exists
+ * a pair of elements that add up to the target.
+ *
+ * Approach: Two Pointer Technique
+ * - Use two pointers: one at the start (left) and one at the end (right)
+ * - If sum of elements at both pointers equals target, pair found
+ * - If sum is less than target, move left pointer forward (to increase sum)
+ * - If sum is greater than target, move right pointer backward (to decrease sum)
+ *
+ * Time Complexity: O(n) - Single pass through the array
+ * Space Complexity: O(1) - No extra space used
+ *
+ * Note: This technique only works on SORTED arrays
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
