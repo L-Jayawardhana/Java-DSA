c36990e69cab63507805ec3c912563d5d8dc0156
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-12-23 23:52:31 +0530

    edit code for get the usesr inputs to the array elements

--- Arrays/src/MaxSubArray/Main.java
+++ Arrays/src/MaxSubArray/Main.java
@@ -1,12 +1,26 @@
 package MaxSubArray;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int[] arr = {2, 1, 5, 1, 3, 2};
-        int k = 3;
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the window size (k): ");
+        int k = scanner.nextInt();
 
         int result = MaxSumSubarray.maxSum(arr, k);
         System.out.println("Maximum sum of subarray: " + result);
+
+        scanner.close();
     }
 }
