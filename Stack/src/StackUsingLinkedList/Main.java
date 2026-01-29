618c40b7d6769fc853b7ec91410ba60865438516
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2026-01-29 20:03:58 +0530

    modify main class to get user inputs

--- Stack/src/StackUsingLinkedList/Main.java
+++ Stack/src/StackUsingLinkedList/Main.java
@@ -1,23 +1,48 @@
 package StackUsingLinkedList;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
         StackLinkedList stack = new StackLinkedList();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+        do {
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Print");
+            System.out.println("5. Exit");
+            System.out.print("Enter choice: ");
+            choice = scanner.nextInt();
 
-        stack.print();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter value to push: ");
+                    int value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    int top = stack.peek();
+                    if (top != -1) {
+                        System.out.println("Top element: " + top);
+                    }
+                    break;
+                case 4:
+                    stack.print();
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        } while (choice != 5);
 
-        stack.pop();
-        stack.print();
-
-        System.out.println("Top element: " + stack.peek());
-
-        stack.pop();
-        stack.pop();
-        stack.pop(); // underflow case
+        scanner.close();
     }
 }
